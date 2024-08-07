eee3f3a504fd9731223c3734fbe88f66c12ff4e3
Jomel Sotelo <119029800+jomelsotelo@users.noreply.github.com>
2024-08-07 13:37:54 -0700

    Changed backend folders

---
+++ backend/src/main/java/com/plp/learning_platform_backend/LearningPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.plp.learning_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/plp/learning_platform_backend/LearningPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plp.learning_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
